76fa584fa89c114ef76530f16848e35515b771ab
Julia-1439 <195843068+Julia-1439@users.noreply.github.com>
2025-11-04 22:04:13 -0800

    Complete knightMoves function

--- src/chessBoard.js
+++ src/chessBoard.js
@@ -38,13 +38,61 @@ class ChessBoard {
       );
   }
 
-  knightMoves(pos1, pos2) {}
+  /**
+   * Returns the shortest path between two positions via only knight movement.
+   * It is a fact that there always exists a knight path between any two
+   * positions in an 8x8 chess board.
+   * @param {Array{Number}} posA [a_i, a_j] representing the starting square
+   * @param {Array{Number}} posB [b_i, b_j] representing the ending square
+   * @returns {Array{Array}} the shortest path from posA and posB, via an array
+   * of indices of the squares in the path
+   */
+  knightMoves([a_i, a_j], [b_i, b_j]) {
+    this.#board.flat().forEach((square) => {
+      square.visited = false;
+      square.prev = null;
+    });
+    const queue = []; // "we have queue at home"
+    let currSquare = this.#board[a_i][a_j];
+    queue.push(currSquare);
+    // Loop can terminate early once found the node belonging to posB
+    while (queue.length > 0) {
+      currSquare = queue.shift();
+      currSquare.visited = true;
+      if (currSquare.i === b_i && currSquare.j === b_j) break; // Since there is provably a knight path to position b, this will execute eventually
+      for (const [neighborI, neighborJ] of currSquare.neighbors) {
+        const neighbor = this.#board[neighborI][neighborJ];
+        if (neighbor.visited) continue;
+        queue.push(neighbor);
+        neighbor.prev = currSquare;
+      }
+    }
 
-  // for testing
+    // Found node belonging to posB
+    const shortestPath = [];
+    let pathTraveller = currSquare;
+    shortestPath.push([pathTraveller.i, pathTraveller.j]);
+    while (pathTraveller.prev) {
+      pathTraveller = pathTraveller.prev;
+      shortestPath.push([pathTraveller.i, pathTraveller.j]);
+    }
+    shortestPath.reverse();
+
+    // Clean up
+    this.#board.flat().forEach((square) => {
+      square.visited;
+      square.prev = null;
+    });
+
+    return shortestPath;
+  }
+
+  // For testing
   get board() {
     return this.#board;
   }
 
+  // For testing
   toString() {
     let s = "";
     this.#board.forEach((row) => {
@@ -61,8 +109,7 @@ class Square {
   constructor(i, j) {
     this.i = i;
     this.j = j;
-    this.edges = [];
-    this.visited = false;
+    this.neighbors = []; // does NOT store Square objects because a square from `this.#board` being modified will not be reflected here in `neighbors`
   }
 
   toString() {
--- src/index.js
+++ src/index.js
@@ -2,10 +2,19 @@
 import { ChessBoard } from "./chessBoard.js";
 
 let G = new ChessBoard();
-console.log(G.toString());
+// console.log(G.toString());
 
-// spot check a few cells
-console.log(G.board[0][0]);
-console.log(G.board[3][3]);
-console.log(G.board[3][0]);
-console.log(G.board[6][7]);
+// spot check a few cells to make sure all connected properly
+// console.log(G.board[0][0]);
+// console.log(G.board[3][3]);
+// console.log(G.board[3][0]);
+// console.log(G.board[6][7]);
+
+// provided test set
+console.log(G.knightMoves([0,0],[3,3]));
+console.log(G.knightMoves([3,3],[0,0]));
+console.log(G.knightMoves([0,0],[7,7]));
+console.log(G.knightMoves([3,3],[4,3]));
+
+// move to same position
+console.log(G.knightMoves([5,5],[5,5]));
